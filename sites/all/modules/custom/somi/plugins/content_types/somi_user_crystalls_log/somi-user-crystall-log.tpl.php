5995b63b1fa7ed41da69c71e6ed16446fa0ea40d
Pavel Ruban <pruban@adyax.com>
2016-04-27 17:33:02 +0600

    refs Implement crystall pages and workaround logic

---
+++ sites/all/modules/custom/somi/plugins/content_types/somi_user_crystalls_log/somi-user-crystall-log.tpl.php
@@ -0,0 +1,9 @@
+<div id="user-crystalls-log">
+  <h2><?php echo $title; ?></h2>
+  <h3>Суммарные данные:</h3>
+  <?php echo $crystalls_total; ?>
+  <h3>История изменения:</h3>
+  <h4 class="crystalls-log">Кредитный счёт <span>Дебетовый счёт:</span></h4>
+  <?php echo $crystalls_log; ?>
+  <?php echo $crystalls_debet_log; ?>
+</div>
