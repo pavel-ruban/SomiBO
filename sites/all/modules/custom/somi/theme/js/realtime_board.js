6fd0378df39433e49a0bf1bc8d0553f78256a54a
Pavel Ruban <pruban@adyax.com>
2016-05-24 17:52:54 +0600

    validation add

--- sites/all/modules/custom/somi/theme/js/realtime_board.js
+++ sites/all/modules/custom/somi/theme/js/realtime_board.js
@@ -3,77 +3,79 @@ var SOMI = SOMI || {};
   // This function resort table to put most recent elements to upper left corner. It works only with one td, marked as
   // most recent element.
   SOMI.somi_board_update = function(td) {
-    var $table = $('table.somi-board');
-    var uid = td.id
-    var selector = 'td#' + uid;
-    var $target_tr = $(selector).parent('tr');
+    if (td && td.id && td.class && td.info) {
+      var $table = $('table.somi-board');
+      var uid = td.id
+      var selector = 'td#' + uid;
+      var $target_tr = $(selector).parent('tr');
 
-    var $next_last_td = undefined;
-    var $target_td = $(selector);
+      var $next_last_td = undefined;
+      var $target_td = $(selector);
 
-    $('table tr').each(function(i, v) {
-      var $cur_tr = $(v);
+      $('table tr').each(function (i, v) {
+        var $cur_tr = $(v);
 
-      // Get last td from row. If we already removed last element in previous iteration use it.
-      var $cur_last_td = $next_last_td
-        ? $next_last_td
-        : $cur_tr.find('td').last('td');
-
-      if ($cur_tr.attr('id') == $target_tr.attr('id')) {
-        $target_td.remove();
-
-        $target_td.find('span').html(td.info);
-        $target_td.find('img').attr('class', td.class);
-        $target_td.attr('class', 'td.class');
-
-        $('tr#1').prepend($target_td);
-
-
-        var promise1 = $target_td.animate(
-          {backgroundColor: '#FFF79F'},
-          1500
-        );
-
-        var promise2 = $target_td.find('img').animate(
-          {opacity: 0.7},
-          1500
-        );
-
-        $.when(promise1, promise2).done(function() {
-          $target_td.animate(
-            {backgroundColor: 'transparent'},
-            1500
-          );
-          $target_td.find('img').animate(
-            {opacity: 1},
-            1500
-          );
-        });
-
-        // Brake each loop.
-        return false;
-      }
-      else {
-        var $next_tr = $cur_tr.next(v);
-
-        if ($next_tr.attr('id') == $target_tr.attr('id')) {
+        if ($cur_tr.attr('id') == $target_tr.attr('id')) {
           $target_td.remove();
-          $next_tr.prepend($cur_last_td);
+
+          $target_td.find('span').html(td.info);
+          $target_td.find('img').attr('class', td.class);
+          $target_td.attr('class', 'td.class');
+
+          $('tr#1').prepend($target_td);
+
+
+          var promise1 = $target_td.animate(
+            {backgroundColor: '#FFF79F'},
+            1500
+          );
+
+          var promise2 = $target_td.find('img').animate(
+            {opacity: 0.7},
+            1500
+          );
+
+          $.when(promise1, promise2).done(function () {
+            $target_td.animate(
+              {backgroundColor: 'transparent'},
+              1500
+            );
+            $target_td.find('img').animate(
+              {opacity: 1},
+              1500
+            );
+          });
+
+          // Brake each loop.
+          return false;
         }
         else {
-          $next_last_td = $next_tr.find('td').last('td');
+          // Get last td from row. If we already removed last element in previous iteration use it.
+          var $cur_last_td = $next_last_td
+            ? $next_last_td
+            : $cur_tr.find('td').last('td');
 
-          $cur_last_td.remove();
-          $next_last_td.remove();
-          $next_tr.prepend($cur_last_td);
+          var $next_tr = $cur_tr.next(v);
+
+          if ($next_tr.attr('id') == $target_tr.attr('id')) {
+            $target_td.remove();
+            $next_tr.prepend($cur_last_td);
+          }
+          else {
+            $next_last_td = $next_tr.find('td').last('td');
+
+            $cur_last_td.remove();
+            $next_last_td.remove();
+            $next_tr.prepend($cur_last_td);
+          }
         }
-      }
-
-    });
+      });
+    }
   }
 
   // Получить полноценный tcp туннель для работы в обе стороны, протокол websocket.
   var socket = io.connect('http://95.172.148.149:8080');
+  //var socket = io.connect('http://127.0.0.1:8080');
 
   // Функция которая сработает когда нам по туннелю через протокол websocket
   // отправят данные как и в случае с ajax.
